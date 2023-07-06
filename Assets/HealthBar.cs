0ab60c2dab1332dc2195cd0cd59e3bdf26e0bdde
Guilherme Jesus <g_bernardo10@hotmail.com>
2023-07-06 20:05:31 -0300

    fix: health bar and start screen

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
